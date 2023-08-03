83368de5233c75f8a9ab1ef3243b05daa26e16a8
vuelesson <374006704@qq.com>
2023-08-03 20:02:00 +0800

    add scss & reset.css

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main">
+    <HelloWorld />
   </main>
 </template>
+
+
+<style lang="scss">
+.main{
+  color:red;
+}
+</style>
