83a251a23b289f03d1dfb23a272e62e47378cbc0
vuelesson <374006704@qq.com>
2023-08-10 20:30:35 +0800

    feat: unplugging-vue-components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,17 +1,18 @@
 <template>
-    <div>
-        hello world
-    </div>
-    <div class="p-4">hello world</div>
-    <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <div>hello world</div>
+  <div class="p-4">hello world</div>
+  <div>{{ msg }}</div>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  <div ref="target">x: {{ x }}, y: {{ y }}, isOutside: {{ isOutside }}</div>
+  <UserHelloWorld></UserHelloWorld>
 </template>
 
 <script setup lang="ts">
+const msg = ref('Hello world11')
 
+const target = ref(null)
+
+const { x, y, isOutside } = useMouseInElement(target)
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,13 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 import UnoCSS from 'unocss/vite'
+
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +21,29 @@ export default defineConfig({
     }),
     vue(),
     vueJsx(),
-    UnoCSS()
+    UnoCSS(),
+    Components({
+      /* options */
+      directoryAsNamespace: true,
+      collapseSamePrefixes: true,
+    }),
+    AutoImport({
+      include: [
+        /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+        /\.vue$/,
+        /\.vue\?vue/, // .vue
+        /\.md$/ // .md
+      ],
+
+      // global imports to register
+      imports: [
+        // presets
+        'vue',
+        // 'vue-router'
+        VueRouterAutoImports,
+        '@vueuse/core'
+      ]
+    })
   ],
   resolve: {
     alias: {
